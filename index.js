7f4ca9211ae9ead9598dd327464e1a8447c89027
itssarthak <sarthakc2211@gmail.com>
2017-07-18 15:00:26 +0530

    Login usingg session created

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ var path =require('path');
 var ejs = require('ejs');
 var mysql = require('mysql');
 var router =express.Router();
+var bcrypt = require('bcryptjs');
+var session = require('express-session');
+var un;
 
-var port=4008;
+var port=4012;
 
 var app = express();
+
+app.use(session({secret: 'ssshhhhh'}));
 
 app.set('view engine','ejs');
 app.use(bodyParser.json());
@@ -19,6 +24,7 @@ var con = mysql.createConnection({
 	password: "123456",
 	database: "mydb"
 });
+
 
 
 con.connect(function(err) {
@@ -97,32 +103,122 @@ app.use(function(req, res, next){
 	next();
 });
 
-app.get('/',function(req,res){
-var userlist=[];
+var sess;
+app.get('/login',function(req,res){
+  sess=req.session;
+  sess.email;
+  if(sess.email){
+    res.redirect('/');
+  }
+  else
+  {
+  res.render('login');
+}
+});
 
 
 
-	con.query("SELECT * FROM comments", function (err, result, fields) {
+app.post('/signup',function(req,res){
+    var UserName = req.body.name;
+    var Email=req.body.email;
+    var Password = req.body.password;
+
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(Password, salt, function(err, hash){
+        Password = hash;
+    console.log(UserName,Email,Password);
+
+      var sql = "INSERT INTO users (Name,Email,Password) VALUES ('"+UserName+"' ,'"+Email+"','"+Password+"')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 User Added");
+
+    res.writeHead(302, {
+  'Location': '/login'
+});
+res.end();
+});
+
+  
+});
+});
+});
+app.post('/log',function(req,res){
+    sess=req.session;
+    username=req.body.User_Name;
+    password= req.body.Password;
+    con.query("SELECT * FROM users", function (err, result, fields) {
     if (err) throw err;
 
+    var user;
     
+    for(var i=0;i<result.length;i++)
+    {
+       user = {
+            'Name':result[i].Name,
+            'Email':result[i].Email,
+            'Password':result[i].Password
+            }
+      if(user.Email==username)
+      {
+        un=user.Name;
+         bcrypt.compare(password, user.Password, function(err, isMatch){
+        if(err) {
+          console.log(err);
+        }
+        if(isMatch){
+          console.log("Match");
+          sess.email=username;
+          res.redirect("/");
+        } else {
+          console.log("Sorry No Match");
+        }
+
+      });
+    } 
+}
+});
+});
+});
+
+app.get('/',function(req,res){
+  sess=req.session;
+  if(sess.email){
+  var userlist=[];
+  con.query("SELECT * FROM comments", function (err, result, fields) {
+    if (err) throw err;
+ 
     var userc;
 
     for(var i=0;i<result.length;i++)
     {
-    	 userc = {
-    	 			'ID':result[i].ID,
-		  			'UserName':result[i].name,
-		  			'UserComment':result[i].comment
-		  	   	}
-		  		userlist.push(userc);
-		  
+       userc = {
+            'ID':result[i].ID,
+            'UserName':result[i].name,
+            'UserComment':result[i].comment
+            }
+          userlist.push(userc);  
     }
-    	res.render('Index',{"userlist":userlist});
+      res.render('Index',{"userlist":userlist,"un":un,"login":true});
   });
+}
+else
+{
+  res.render('index',{"login":false})
+}
 
 });
+
+app.get('/logout',function(req,res){
+req.session.destroy(function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    res.redirect('/login');
+  }
 });
+});
+
 app.listen(port);
 
 console.log('Server started on port '+port);
